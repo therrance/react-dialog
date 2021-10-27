53012249f36e42cf3fb8f91fc0358be937162217
Taras Gordiienko <lozuvatka@gmail.com>
2021-10-27 16:44:45 +0300

    Add themes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Dialog from "./components/Dialog";
 import Sender from "./components/Sender";
 
+import { AppContext } from "./context";
+import { getTheme, setTheme } from "./helpers/theme";
+import { useEffect } from "react/cjs/react.development";
+
 const App = () => {
+  const [message, addMessage] = useState(null);
+  const [theme, toggleTheme] = useState(getTheme());
+
+  useEffect(() => {
+    setTheme(theme);
+  }, [theme]);
+
   return (
-    <div className="container">
-      <Header />
-      <Dialog />
-      <Sender />
-    </div>
+    <AppContext.Provider value={{ theme, toggleTheme }}>
+      <div className="container">
+        <Header />
+        <Dialog newMessage={message} />
+        <Sender onAddMessage={addMessage} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import UserInfo from "./UserInfo";
+import Theme from "./Theme";
 
 import "./styles.css";
 
 const Header = () => (
   <div className="header">
     <UserInfo />
+    <Theme />
   </div>
 );
 
